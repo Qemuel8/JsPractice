1364bc0af3b939738f2508bf0b17ebd43c151657
Yigit Alsancak <yigitalsancak8@gmail.com>
2022-09-12 08:38:29 +0300

    point and shoot game completed

--- Js Game/Javascript Game/script.js
+++ Js Game/Javascript Game/script.js
@@ -2,21 +2,18 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 const collisionCanvas = document.getElementById('collisionCanvas');
 const collisionCtx = collisionCanvas.getContext('2d');
 collisionCanvas.width = window.innerWidth;
 collisionCanvas.height = window.innerHeight;
 
-
-
 let score = 0;
+let gameOver = false;
 ctx.font = '50px Impact';
 
 let timeToNextRaven = 0;
 let ravenInterval = 500;
 let lastTime = 0;
-
 
 let ravens = [];
 class Raven {
@@ -36,10 +33,10 @@ class Raven {
         this.frame = 0;
         this.maxFrame = 4;
         this.timeSinceFlap = 0;
-        this.flapInterval = Math.random() * 50 + 50;
+        this.flapInterval = Math.random() * 30 + 40;
         this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
         this.color = 'rgb(' + this.randomColors[0] + ',' +  this.randomColors[1]+ ',' + this.randomColors[2] + ')';
-
+        this.hasTrail = Math.random() > 0.5;
 
     }
     update(deltatime){
@@ -55,9 +52,15 @@ class Raven {
             if(this.frame> this.maxFrame) this.frame = 0;
             else this.frame++;
             this.timeSinceFlap = 0;
+            if(this.hasTrail){
+                for(let i = 0; i<5; i++){
+                  particles.push(new Particle(this.x, this.y, this.width, this.color));
+
+                }
+            }
+            
         }
-
-
+        if(this.x < 0 - this.width) gameOver = true;
     }
     draw(){
         collisionCtx.fillStyle = this.color;
@@ -65,13 +68,77 @@ class Raven {
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
+let explosions = [];
+class Explosion {
+    constructor(x, y, size){
+        this.image = new Image();
+        this.image.src = 'boom.png';
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.frame = 0;
+        this.sound = new Audio();
+        this.sound.src= 'boom.wav';
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 100;
+        this.markedForDeletion = false;
+    }
+    update(deltatime){
+        if(this.frame === 0) this.sound.play();
+        this.timeSinceLastFrame += deltatime;
+        if(this.timeSinceLastFrame > this.frameInterval){
+            this.frame++;
+            this.timeSinceLastFrame = 0;
+            if(this.frame > 5) this.markedForDeletion = true;
+        }
+    }
+    draw(){
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size/4, this.size, this.size)
+    }
+}
+let particles = [];
+class Particle {
+    constructor( x, y, size, color){
+        this.size = size;
+        this.x = x + this.size/2;
+        this.y = y + this.size/3;
+        this.radius = Math.random() * this.size/10;
+        this.maxRadius = Math.random() * 20 + 35;
+        this.markedForDeletion = false;
+        this.speedx = Math.random()* 1 + 0.5;
+        this.color = color;
+    }
+    update(){
+        this.x += this.speedx;
+        this.radius += 0.4;
+        if(this.radius > this.maxRadius - 5) this.markedForDeletion = true;
+    }
+    draw(){
+        ctx.save();
+        ctx.globalAlpha = 1 - this.radius/this.maxRadius;
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2);
+        ctx.fill();
+        ctx.restore();
+    }
+}
 
-function drawScore()
-{
+function drawScore(){
     ctx.fillStyle = 'black';
     ctx.fillText('Score: ' + score, 50,75);
     ctx.fillStyle = 'white';
     ctx.fillText('Score: ' + score, 55,80)
+}
+
+function drawGameOver(){
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'black';
+    ctx.fillText('GAME OVER, your score is: ' + score, canvas.width/2, canvas.height/2 );
+    ctx.fillStyle = 'white';
+    ctx.fillText('GAME OVER, your score is: ' + score, canvas.width/2 + 5, canvas.height/2 + 5 );
 }
 
 window.addEventListener('click', function(e){
@@ -81,8 +148,10 @@ window.addEventListener('click', function(e){
     ravens.forEach(object => {
         if(object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2])
         {
+            //collisiondetected
             object.markedForDeletion = true;
             score++;
+            explosions.push(new Explosion(object.x, object.y, object.width))
         }
     })
 
@@ -103,10 +172,13 @@ function animate(timestamp){
 
     };
     drawScore();
-[...ravens].forEach(object => object.update(deltatime));
-[...ravens].forEach(object => object.draw());
-ravens = ravens.filter( object => !object.markedForDeletion)
+[...particles, ...ravens, ...explosions].forEach(object => object.update(deltatime));
+[ ...particles, ...ravens, ...explosions].forEach(object => object.draw());
+ravens = ravens.filter( object => !object.markedForDeletion);
+explosions = explosions.filter( object => !object.markedForDeletion);
+particles = particles.filter( object => !object.markedForDeletion)
 
-requestAnimationFrame(animate);
+if(!gameOver) requestAnimationFrame(animate);
+else drawGameOver();
 }
 animate(0);
